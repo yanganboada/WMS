3984e0396428e0e2e93149ff614be7dd63055ea1
Joanna Pham <jopham90@gmail.com>
2020-11-10 16:43:44 -0800

    created back end for grabbing list of products

--- server/index.js
+++ server/index.js
@@ -14,9 +14,28 @@ app.use(sessionMiddleware);
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
-  db.query(`select 'successfully connected' as "message"`)
+  db.query('select \'successfully connected\' as "message"')
     .then(result => res.json(result.rows[0]))
     .catch(err => next(err));
+});
+
+app.get('/api/products', (req, res, next) => {
+  const sql = `
+    select "p"."sku",
+           "p"."name",
+           "c"."categoryName" as "category",
+           "p"."qty"
+      from "products" as "p"
+      join "category" as "c" using ("categoryId")
+  `;
+
+  db.query(sql)
+    .then(result => {
+      const product = result.rows;
+      res.status(200).json(product);
+    })
+    .catch(err => console.error(err));
+
 });
 
 app.use('/api', (req, res, next) => {
